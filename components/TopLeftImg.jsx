117fb1dcd2765532929ac230b9b101f613b373ab
Bettahi <bettahiabdou@gmail.com>
2024-04-19 17:28:27 +0200

    first commit

---
+++ components/TopLeftImg.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+const TopLeftImg = () => {
+  return (
+      <div className="absolute left-0 top-0 z-10 w-[200px] xl:w-[400px] opacity-50 pointer-events-none select-none"
+           >
+          <Image
+              src="/top-left-img.png"
+              alt="left cover bg"
+              width={400}
+              height={400}
+          />
+      </div>
+
+  );
+};
+
+export default TopLeftImg;
