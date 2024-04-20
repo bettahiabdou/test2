97cef5e649753fdcf1d8d98077c427e39caf5fc7
Bettahi <bettahiabdou@gmail.com>
2024-04-20 15:41:05 +0200

    new chnages10

--- pages/services/index.jsx
+++ pages/services/index.jsx
@@ -8,7 +8,7 @@ export const serviceData = [];
 
 const Services = () => {
     return (
-        <div className=" overflow-y-scroll sm:h-full sm:bg-primary/60 py-36 flex items-center" style={{maxHeight: "150vh"}}>
+        <div className=" overflow-y-scroll sm:h-full sm:bg-primary/60  sm:flex items-center" style={{maxHeight: "150vh"}}>
             <Circles/>
             <div className="container mx-auto">
                 <div className="flex flex-col xl:flex-row gap-x-8 items-start">
@@ -91,7 +91,28 @@ const Services = () => {
                     </motion.p>
                 </div>
 
-
+                <div className="text-center flex flex-col lg:text-left mb-4 xl:mb-0 xl:w-[30vw] xl:hidden"
+                     style={{visibility: "hidden"}}>
+                    <motion.h2
+                        variants={fadeIn("up", 0.2)}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className="h2 xl:mt-8"
+                    >
+                        Nos services<span style={{color: "#51ADE5"}}>.</span>
+                    </motion.h2>
+                    <motion.p
+                        variants={fadeIn("up", 0.4)}
+                        initial="hidden"
+                        animate="show"
+                        exit="hidden"
+                        className="mb-4 max-w-[400px] mx-auto lg:mx-0"
+                    >
+                        Découvrez comment nos services de branding, design, développement, rédaction et SEO peuvent
+                        transformer votre présence en ligne et propulser votre entreprise vers de nouveaux sommets
+                    </motion.p>
+                </div>
             </div>
             <Bulb/>
 
