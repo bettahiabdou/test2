8c888624cd20cac9ba582d028d35f36ada8c596e
Bettahi <bettahiabdou@gmail.com>
2024-04-20 14:39:33 +0200

    new chnages3

--- components/Header.jsx
+++ components/Header.jsx
@@ -3,29 +3,29 @@ import Link from "next/link";
 import Socials from "../components/Socials";
 
 const Header = () => {
-  return (
-      <header className="w-full items-center px-16 xl-px-0 xl:h-[90px]">
-        <div className="container mx-auto">
-          <div className="flex flex-col lg:flex-row justify-between items-center gap-y-6 py-8">
-            {/* logo */}
-            <Link href="/">
-              <Image
-                  src="/logo.svg"
-                  alt="logo"
-                  width={300}
-                  height={20}
-                  priority
-              />
-            </Link>
+    return (
+        <header className="w-full items-center px-4 lg:px-16 xl:px-0 py-4 lg:h-[90px]">
+            <div className="container mx-auto">
+                <div className="flex flex-col lg:flex-row justify-between items-center gap-y-4 lg:gap-y-0 lg:py-2">
+                    {/* logo */}
+                    <Link href="/">
+                        <div className="w-full lg:w-auto">
+                            <Image
+                                src="/logo.svg"
+                                alt="logo"
+                                width={300}
+                                height={20}
+                                priority
+                            />
+                        </div>
+                    </Link>
 
-            {/* socials */}
-            <Socials />
-          </div>
-        </div>
-
-
-      </header>
-  );
+                    {/* socials */}
+                    <Socials />
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
