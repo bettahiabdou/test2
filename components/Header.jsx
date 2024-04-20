a26794d6b92782d54521ec2af7797a667d26323d
Bettahi <bettahiabdou@gmail.com>
2024-04-20 14:51:17 +0200

    new chnages7

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,11 +5,7 @@ import Socials from "../components/Socials";
 const Header = () => {
     return (
         <header className="w-full items-center px-4 lg:px-16 xl:px-0 py-4 lg:h-[90px]">
-            <meta name="theme-color" content="#ffffff"/>
-            <!-- Windows Phone -->
-            <meta name="msapplication-navbutton-color" content="#ffffff"/>
-            <!-- iOS Safari -->
-            <meta name="apple-mobile-web-app-status-bar-style" content="#ffffff"/>
+
             <div className="container mx-auto">
                 <div className="flex flex-col lg:flex-row justify-between items-center gap-y-4 lg:gap-y-0 lg:py-2">
                     {/* logo */}
@@ -26,7 +22,7 @@ const Header = () => {
                     </Link>
 
                     {/* socials */}
-                    <Socials/>
+                    <Socials />
                 </div>
             </div>
         </header>
